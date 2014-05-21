94f8bb063f6fc2f4eea08a94b6431f437a118146
Dnyanraj Bodhe <dnyanraj@gmail.com>
2014-05-21 05:56:32 -0500

    Added solution to PA2-Q2
    Added plot2.R, plot2.png

--- ExploratoryDataAnalysis/PA2/plot2.R
+++ ExploratoryDataAnalysis/PA2/plot2.R
@@ -10,17 +10,17 @@ SCCData<- readRDS(filePath)
 
 #-=-=-=-=
 
-#Q1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
-#Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
-x1sub <- subset(NEIData, year>= 1999, select = c(Emissions, year))
+#Q2: Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008?
+#Use the base plotting system to make a plot answering this question.
+x1sub <- subset(NEIData, fips=="24510", select = c(Emissions, year))
 x1 <- aggregate(Emissions~year, x1sub, sum)
 
 #Create png device with specified specifications
-png(file="plot1.png")
+png(file="plot2.png")
 
 #Create plot
-plot(x1, main = "Total PM2.5 Emmissions By Year", xlab = "Year", ylab = "Total Emmissions in Tons")
-lines(x1$year, x1$Emissions, type='l', col="blue")
+plot(x1, main = "Total PM2.5 Emmissions By Year for Baltimore City, Maryland", xlab = "Year", ylab = "Total Emmissions in Tons")
+lines(x1$year, x1$Emissions, type='l', col="red")
 
 #Turn device off. If this step is not executed, the file won't be accessible for viewing
 dev.off()
